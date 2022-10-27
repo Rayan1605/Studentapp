4cdd7257e25a83b8214f69019740e879b7479b4e
rayan <rayanzyad99@gmail.com>
2022-10-27 12:45:54 +0100

    Added a new file called studentapp

---
+++ src/main/java/ie/atu/StudentApp.java
@@ -0,0 +1,31 @@
+package ie.atu;
+
+public class StudentApp {
+    private  String Name;
+    private String email;
+    private String studentemail;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStudentemail() {
+        return studentemail;
+    }
+
+    public void setStudentemail(String studentemail) {
+        this.studentemail = studentemail;
+    }
+}
