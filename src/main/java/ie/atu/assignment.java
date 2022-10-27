c50e2e3b7119e5cff5e3925df305cd660b9dd431
rayan <rayanzyad99@gmail.com>
2022-10-27 12:47:47 +0100

    Added a new file called studentapp

--- src/main/java/ie/atu/assignment.java
+++ src/main/java/ie/atu/assignment.java
@@ -1,4 +1,23 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class assignment {
+    public static void main(String[] args) {
+        Scanner myinput = new Scanner(System.in);
+        String Name ,email,nameofcourse;
+
+            System.out.println("Please enter your name ");
+            Name = myinput.next();
+            System.out.println();
+            System.out.println("Please enter your email ");
+            email = myinput.next();
+            System.out.println("Please enter your course");
+            nameofcourse = myinput.next();
+
+
+
+
+
+    }
 }
